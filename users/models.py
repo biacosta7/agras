83ea9709b223f8e9b4acfb2d1d8c133a355529d2
Vinicius <vini.ventura999@gmail.com>
2024-09-24 14:53:13 -0300

    removing db fields

--- users/models.py
+++ users/models.py
@@ -2,10 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    endereco = models.CharField(max_length=255, blank=False, null=False)
     cidade = models.CharField(max_length=80, blank=False, null=False)
     estado = models.CharField(max_length=80, blank=False, null=False)
-    cep = models.CharField(max_length=10, blank=False, null=False)
     data_nascimento = models.DateField(null=True, blank=True)
     # foto_perfil = models.ImageField(upload_to='perfil/', blank=True, null=True) VER COMO VAMOS ARMAZENAR IMAGENS NO DB (acredito que o SQLite não seja muito eficiente no 
                                                                                                                                                 # armanzenamento de imagens)
