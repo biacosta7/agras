20182a213f82713b6d70924cbb648881b8d0153c
William Souza <66651052+william-mjsouza@users.noreply.github.com>
2024-09-25 15:20:14 -0300

    Criação da app que renderiza a home page

--- agras/settings.py
+++ agras/settings.py
@@ -32,13 +32,14 @@ AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'America/Recife'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -122,6 +123,22 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Armazenamento de arquivos estáticos (diferente para desenvolvimento e produção)
+if DEBUG:
+    # Configuração padrão para desenvolvimento
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    # Toda vez que for modificado algum arquivo .css, deve-se usar o comando: 
+    # python manage.py collectstatic
+else:
+    # Configuração para produção
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+# Configuração de ALLOWED_HOSTS
+if DEBUG:
+    ALLOWED_HOSTS = ['*']  # Permite qualquer host para testes em desenvolvimento
+else:
+    ALLOWED_HOSTS = ['meusite.com', 'www.meusite.com']  # Defina os hosts reais da sua aplicação em produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- agras/urls.py
+++ agras/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')), # Inclui as URLs da app 'core' para a home page ('' = http://127.0.0.1:8000/)
     path('auth/', include('users.urls')),
 ]
