9eeb39ee77ba3bd31352ffffbd6d7721e165d9ed
vinivent <vini.ventura999@gmail.com>
2024-10-01 09:45:02 -0300

    communities urls

--- communities/urls.py
+++ communities/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('hub/', views.community_list, name='community_hub'),
     path('criar/', views.create_community, name='create_community'),
-    path('editar/<int:id>/', views.update_community, name='update_community'),
-    path('deletar/<int:id>/', views.delete_community, name='delete_community')
+    path('editar/<int:pk>/', views.update_community, name='update_community'),
+    path('deletar/<int:pk>/', views.delete_community, name='delete_community')
 ]
