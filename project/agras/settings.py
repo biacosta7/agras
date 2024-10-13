3efb9ae80015044955c659e6c9c77000897fb1e3
biacosta7 <contact.beatrizcosta@gmail.com>
2024-10-13 09:27:58 -0300

    fixing static not loading

--- project/agras/settings.py
+++ project/agras/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/assets/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
