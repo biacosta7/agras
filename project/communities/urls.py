57cca08fed56c9575f2aab210ff5e9a93fb5d913
Vinicius <vini.ventura999@gmail.com>
2024-10-15 01:05:53 -0300

    communities urls

--- project/communities/urls.py
+++ project/communities/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('editar/<int:pk>/', views.update_community, name='update_community'),
     path('deletar/<int:pk>/', views.delete_community, name='delete_community'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('community/<int:community_id>/', views.community_detail, name='community_detail'),  # Rota dinâmica
 ]
