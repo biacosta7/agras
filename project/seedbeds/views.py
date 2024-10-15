345df9f355620f755d5ac3cb9c333f9208d30d0e
Vinicius <vini.ventura999@gmail.com>
2024-10-15 01:07:48 -0300

    seedbeds views

--- project/seedbeds/views.py
+++ project/seedbeds/views.py
@@ -1,33 +1,57 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.exceptions import ValidationError
 from seedbeds.models import Seedbed
-from products.models import Product
+from communities.models import Community  
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def list_seedbeds(request):
-    # Obtém todos os canteiros
-    canteiros = Seedbed.objects.all()  # Busca todos os canteiros
-    return render(request, 'list_seedbeds.html', {'canteiros': canteiros})
+    community = get_object_or_404(Community, id=request.user.community.id)  
+    canteiros = Seedbed.objects.filter(community=community)  
+    return render(request, 'list_seedbeds.html', {'canteiros': canteiros, 'community': community})
 
+@login_required
 def create_seedbed(request):
+    user = request.user
+    communities = user.communities_members.all()  # Obtém todas as comunidades do usuário
+
     if request.method == 'POST':
-        nome = request.POST.get('nome')  # Obtém o nome do Seedbed a partir do request
+        # Supondo que você tenha um formulário que inclui a seleção da comunidade
+        community_id = request.POST.get('community')  # O ID da comunidade selecionada
+        community = Community.objects.get(id=community_id)
+
+        # Criação do canteiro
+        seedbed_name = request.POST.get('seedbed_name')
+        Seedbed.objects.create(name=seedbed_name, community=community)
+
+        return redirect('list-seedbeds')  # Redirecione para a lista de canteiros após a criação
+
+    return render(request, 'create_seedbed.html', {'communities': communities})
+
+@login_required
+def edit_seedbed(request, seedbed_id):
+    seedbed = get_object_or_404(Seedbed, id=seedbed_id)
+    community = get_object_or_404(Community, id=request.user.community.id)  
+
+    if request.method == 'POST':
+        nome = request.POST.get('nome')  
         if nome:
-            if Seedbed.objects.filter(nome=nome).exists():
-                messages.error(request, 'Já existe um canteiro com esse nome. Por favor, tente novamente.')
-                return render(request, 'create_seedbed.html') 
+            if Seedbed.objects.filter(nome=nome, community=community).exclude(id=seedbed_id).exists():
+                messages.error(request, 'Já existe um canteiro com esse nome nesta comunidade. Por favor, tente novamente.')
+                return render(request, 'list_seedbeds.html', {'canteiros': Seedbed.objects.filter(community=community), 'community': community}) 
             else:
-                Seedbed.objects.create(nome=nome)  
-                messages.success(request, 'Canteiro criado com sucesso!')
-                return redirect('seedbeds:list-seedbeds') 
-    return render(request, 'create_seedbed.html') # Renderiza o formulário vazio se não for POST
+                seedbed.nome = nome  
+                seedbed.save()  
+                messages.success(request, 'Canteiro editado com sucesso!')
+                return redirect('list-seedbeds')  
 
+    return render(request, 'list_seedbeds.html', {'canteiros': Seedbed.objects.filter(community=community), 'community': community})  
+
+@login_required
 def delete_seedbed(request, seedbed_id):
-    seedbed = get_object_or_404(Seedbed, id=seedbed_id)  # Obtém o Seedbed com base no ID
+    seedbed = get_object_or_404(Seedbed, id=seedbed_id)  
     if request.method == 'POST':
-        seedbed.delete()  # Deleta o Seedbed
+        seedbed.delete() 
         messages.success(request, 'Canteiro deletado com sucesso!')
-        return redirect('seedbeds:list-seedbeds')  # Redireciona para a lista de Seedbeds
-
-
-    return render(request, 'confirm_delete.html', {'seedbed': seedbed})
+        return redirect('seedbeds:list-seedbeds') 
+    return render(request, 'list_seedbeds.html', {'canteiros': Seedbed.objects.filter(community=request.user.community), 'community': request.user.community}) 
