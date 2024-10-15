191a660063d46aef288f32a9266fc198f6d0c300
Vinicius <vini.ventura999@gmail.com>
2024-10-15 01:06:35 -0300

    products urls

--- project/products/urls.py
+++ project/products/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-from .views import(
-	product_detail_view, 
-	product_delete_view, 
-	product_list_view, 
-	product_update_view,
+from .views import (
     create_product_view, 
     create_typeproduct_view
 )
@@ -11,10 +7,6 @@ from .views import(
 app_name = 'product'
 
 urlpatterns = [
-    path('adicionar/<int:seedbed_id>/', create_product_view, name='create_product'),
-    path('cadastrar/<int:seedbed_id>/', create_typeproduct_view, name='create_typeproduct'),
-    path('editar/<int:seedbed_id>/<int:product_id>/', product_update_view, name='product-update'),
-	path('listar/<int:seedbed_id>/', product_list_view, name='product-list'), 
-	path('<int:seedbed_id>/<int:product_id>/delete/', product_delete_view, name='product-delete'),
-    path('details/<int:id>/', product_detail_view, name='product-detail'),
+    path('adicionar/<int:community_id>/', create_product_view, name='create_product'),
+    path('cadastrar/<int:community_id>/', create_typeproduct_view, name='create_typeproduct'),
 ]
