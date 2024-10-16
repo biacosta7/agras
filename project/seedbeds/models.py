54af4c708d638b8f67906452a1b778de455685bc
Vinicius <vini.ventura999@gmail.com>
2024-10-16 15:46:35 -0300

    chore: adding null true to area field and removing comment

--- project/seedbeds/models.py
+++ project/seedbeds/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from areas.models import Area
 class Seedbed(models.Model):
     nome = models.CharField(max_length=80, blank=False, null=False)
-    #community = models.ForeignKey('communities.Community', on_delete=models.CASCADE, related_name='seedbeds_in_community', null=True, blank=True)
-    area = models.ForeignKey(Area, related_name='seedbeds', on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, related_name='seedbeds', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nome
