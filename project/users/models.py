1abd7d286f600702fb8c20269b6f72a32842c175
biacosta7 <contact.beatrizcosta@gmail.com>
2024-10-17 12:38:41 -0300

    city and state - null=true

--- project/users/models.py
+++ project/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    city = models.CharField(max_length=80, blank=False, null=False)
-    state = models.CharField(max_length=80, blank=False, null=False)
+    city = models.CharField(max_length=80, blank=False, null=True)
+    state = models.CharField(max_length=80, blank=False, null=True)
     # profile_pic = models.ImageField(upload_to='perfil/', blank=True, null=True) VER COMO VAMOS ARMAZENAR IMAGENS NO DB (acredito que o SQLite não seja muito eficiente no 
                                                                                                                                                 # armanzenamento de imagens)
 
