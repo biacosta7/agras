dff5f423dd51fd07563f96effbc08865bdbf799b
biacosta7 <contact.beatrizcosta@gmail.com>
2024-11-02 12:32:00 -0300

    create question view

--- project/chat/views.py
+++ project/chat/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.contrib.auth.decorators import login_required
+from .models import ChatBot
+from django.http import HttpResponseRedirect, JsonResponse
+import google.generativeai as genai
+import os
 
-# Create your views here.
+genai.configure(api_key=os.environ["API_KEY"])
+
+@login_required
+def ask_question(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        model = genai.GenerativeModel("gemini-pro")
+        chat = model.start_chat()
+        response = chat.send_message(text)
+        user = request.user
+        ChatBot.objects.create(text_input=text, gemini_output=response.text, user=user)
+
+        response_data = {
+            "text": response.text,
+        }
+        return JsonResponse({"data": response_data})
+    else:
+        return HttpResponseRedirect(
+            #reverse("chat")
+        ) 
+
+@login_required
+def chat(request):
+    user = request.user
+    chats = ChatBot.objects.filter(user=user)
+    return render(request, "chat_bot.html", {"chats": chats})
