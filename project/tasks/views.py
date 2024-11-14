b1e5a006b94d6bc55ba813d0e91439cf29d5bea3
biacosta7 <contact.beatrizcosta@gmail.com>
2024-11-14 05:43:02 -0300

    fixed added task error

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -9,16 +9,10 @@ from seedbeds.models import Seedbed
 from communities.models import Community
 from django.urls import reverse
 from django.contrib import messages
-
+from django.http import JsonResponse
 
 def add_task(request, community_id, area_id=None, seedbed_id=None, product_id=None, type_product_id=None):
-    task_type = request.POST.get('type')
-    
     community = get_object_or_404(Community, id=community_id)
-    # area = get_object_or_404(Area, id=area_id, community=community) if area_id else None
-    # seedbed = get_object_or_404(Seedbed, id=seedbed_id, area=area) if seedbed_id else None
-    # product = get_object_or_404(Product, id=product_id, seedbed=seedbed) if product_id else None
-    # type_product = get_object_or_404(TypeProduct, id=type_product_id) if type_product_id else None
     users = community.members.all().union(community.admins.all()) if community else None
 
     if request.method == 'POST':
@@ -30,18 +24,20 @@ def add_task(request, community_id, area_id=None, seedbed_id=None, product_id=No
         status = request.POST.get('status')
         priority = request.POST.get('priority')
         responsible_user_ids = request.POST.getlist('responsible_users[]')
+        print(request.POST)  # Adiciona esta linha para inspecionar os dados recebidos
 
-
-        #fazer recorrencia nao ser obrigatoria
-        if not title or not deadline or not recurrence:
-            return HttpResponseBadRequest("Todos os campos são obrigatórios.")
-
+        if not title or not deadline:
+            print('Título e data de vencimento são obrigatórios.')
+            return JsonResponse({'error': 'Título e data de vencimento são obrigatórios.'}, status=400)
+        
         try:
-            deadline = timezone.datetime.strptime(deadline, '%d/%m/%Y')  # Formato de data sem hora
+            deadline = timezone.datetime.strptime(deadline, '%d-%m-%Y')
         except ValueError:
-            return HttpResponseBadRequest("Formato inválido. Use 'DD/MM/AAAA'.")
+            print("Formato de data inválido. Use 'DD/MM/AAAA")
+            return JsonResponse({'error': "Formato de data inválido. Use 'DD/MM/AAAA'."}, status=400)
+
+        # Cria a tarefa
         task = Task(
-            #type=task_type,
             title=title,
             description=description,
             is_completed=is_completed,
@@ -50,52 +46,30 @@ def add_task(request, community_id, area_id=None, seedbed_id=None, product_id=No
             status=status,
             priority=priority,
         )
-
-        # if task_type == 'community' and community:
-        #     task.community = community
-        # elif task_type == 'area' and area:
-        #     task.area = area
-        # elif task_type == 'seedbed' and seedbed:
-        #     task.seedbed = seedbed
-        # elif task_type == 'product' and product:
-        #     task.product = product
-        # elif task_type == 'type_product' and type_product:
-        #     task.type_product = type_product
-        # else:
-        #     return HttpResponseBadRequest("Tipo inválido ou não encontrado.")
-
         task.save()
 
-        # Then set the many-to-many relationship
+        # Configura o relacionamento many-to-many para usuários responsáveis
         if responsible_user_ids:
-            # Convert IDs to integers and filter out empty strings
             user_ids = [int(uid) for uid in responsible_user_ids if uid]
-            # Get the user objects
             users = User.objects.filter(id__in=user_ids)
-            # Set the many-to-many relationship
             task.responsible_users.set(users)
 
+        # Resposta AJAX de sucesso
         messages.success(request, "Tarefa adicionada com sucesso!")
-        # Gerar a URL para redirecionar para o dashboard, com os parâmetros necessários
-        redirect_url = reverse('dashboard', kwargs={'community_id': community_id})
-        return redirect(redirect_url)
+        return JsonResponse({'success': 'Tarefa adicionada com sucesso!'})
 
-    # Criar um dicionário de kwargs, omitindo parâmetros que são None
-    kwargs = {}
-    if community_id:
-        kwargs['community_id'] = community_id
-    if area_id:
-        kwargs['area_id'] = area_id
-    if seedbed_id:
-        kwargs['seedbed_id'] = seedbed_id
-    if product_id:
-        kwargs['product_id'] = product_id
-    if type_product_id:
-        kwargs['type_product_id'] = type_product_id
+    # Geração da URL para o formulário
+    kwargs = {key: val for key, val in [
+        ('community_id', community_id),
+        ('area_id', area_id),
+        ('seedbed_id', seedbed_id),
+        ('product_id', product_id),
+        ('type_product_id', type_product_id)
+    ] if val is not None}
 
-    # Gerar a URL para o formulário
     form_url = reverse('add_task', kwargs=kwargs)
 
+    # Renderiza o modal de formulário via AJAX
     context = {
         'form_url': form_url,
         'product_id': product_id,
@@ -106,7 +80,10 @@ def add_task(request, community_id, area_id=None, seedbed_id=None, product_id=No
         'users': users
     }
 
-    return render(request, 'add_task.html', context)
+    if request.is_ajax():
+        return render(request, 'add_task.html', context)
+
+    return render(request, 'dashboard.html', context)
 
 # def view_tasks(request, community_id=None, area_id=None, seedbed_id=None, product_id=None, type_product_id=None):
 #     tasks = Task.objects.all()
