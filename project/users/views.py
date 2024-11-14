5b02c50cec925732c652f44ec334f70e68636121
AdrianMichael5 <adrian_michael_araujo@hotmail.com>
2024-11-14 19:15:12 -0300

    att views.py

--- project/users/views.py
+++ project/users/views.py
@@ -79,12 +79,6 @@ def delete_user(request, user_id):
     messages.success(request, 'Usuário deletado com sucesso.')
     return redirect('get_all_users')
 
-
-@login_required
-def get_all_users(request):
-    users = User.objects.all()
-    return render(request, 'listar_usuarios.html', {'users': users})
-
 def login(request):
     if request.user.is_authenticated:
         return redirect('community_hub')
